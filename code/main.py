0ef8f29375d9ef693c15dda2827cadcaa64ff46b
olegogonich <olegogonich240708@gmail.com>
2024-07-19 17:32:26 +0300

    init

---
+++ code/main.py
@@ -0,0 +1,40 @@
+import asyncio
+from asyncio.exceptions import CancelledError
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+import config as cf
+from code.util import logger
+# routers
+from code.command.start.start_command import router as start_command_router
+from code.command.register.register_command import router as register_command_router
+
+routers = [
+    start_command_router,
+    register_command_router
+]
+
+dp = Dispatcher()
+
+
+async def include_routers() -> None:
+    for router in routers:
+        dp.include_router(router)
+
+
+async def main() -> None:
+    bot = Bot(token=cf.TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await include_routers()
+
+    try:
+        logger.info('bot is running!')
+        await dp.start_polling(bot)
+    except (CancelledError, KeyboardInterrupt, SystemExit):
+        dp.shutdown()
+        logger.info('stopping')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
