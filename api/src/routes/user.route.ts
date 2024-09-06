0c3ffbf6f7561833a3487d173964e64b0680ff91
Sima <mksim3uchnev@gmail.com>
2024-09-06 16:50:07 +0300

    fix

--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -7,7 +7,7 @@ import roles from '../config/roles';
 const router = Router();
 
 router.route('/setRole').post(verifyToken.auth, verifyRole(roles.ADMIN), userController.setRole);
-router.route('/').get(userController.getAll);
+router.route('/').get(verifyToken.auth, userController.getAll);
 router.route('/:userId').delete(verifyToken.auth, userController.destroy);
 
 export default router;
