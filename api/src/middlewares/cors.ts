073ccf4331023913f1771a72a56eb8e95d4e0823
Simai1 <mksim3uchnev@gmail.com>
2024-11-11 22:11:07 +0300

    fix(docker): prepare docker for ghcr

--- api/src/middlewares/cors.ts
+++ api/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 export default cors({
     credentials: true,
-    origin: process.env.WEB_URL,
+    origin: process.env.WEB_URL || '*',
     exposedHeaders: '*',
     allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
