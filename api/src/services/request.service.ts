4ce6f7a71bef9475c1b5e8aea4f6ed73ec41452d
Sima <mksim3uchnev@gmail.com>
2025-03-02 17:34:28 +0300

    fix(requests): fix autoCreated query param

--- api/src/services/request.service.ts
+++ api/src/services/request.service.ts
@@ -115,7 +115,7 @@ const getAllRequests = async (filter: any, order: any, pagination: any) => {
                   ? { [Op.notIn]: value }
                   : { [Op.in]: value };
         } else if (fieldName === 'isAutoCreated') {
-            whereParams[fieldName] = { [Op.is]: true };
+            whereParams[fieldName] = { [Op.is]: value === 'true' };
         } else {
             whereParams[fieldName] = isExclusion ? { [Op.notIn]: value } : { [Op.in]: value };
         }
