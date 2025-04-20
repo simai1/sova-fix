852b090ae6f502c1515ee9330f3b13fa734a493a
Dmitry <a.d1mon@yandex.ru>
2025-04-21 00:12:33 +0300

    fix(front): заливаем изменения 2
    поправил адаптив в header, добавил адаптив для справочников, главной страницы и маршрутного листа до 320px

--- front/src/UI/Layout/Layout.jsx
+++ front/src/UI/Layout/Layout.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Layout.module.scss'; // Импортируем стили
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+    const [isCardPage, setIsCardPage] = useState(false)
+
+    useEffect(() => {
+        if (location.pathname === '/CardPage/Card') setIsCardPage(true)
+    }, [location])
+
     return (
-        <div className={styles.layout__container}>
+        <div className={isCardPage ? `${styles.layout__container} ${styles.layout__container__card}` : styles.layout__container}>
             {children}
         </div>
     );
