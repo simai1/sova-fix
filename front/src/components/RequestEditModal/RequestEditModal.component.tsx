718a380a0e01040cee7b0a6faabda6671bdff325
Dmitry <a.d1mon@yandex.ru>
2026-01-31 00:15:55 +0300

    refactor(client): исправлен порядок полей

--- front/src/components/RequestEditModal/RequestEditModal.component.tsx
+++ front/src/components/RequestEditModal/RequestEditModal.component.tsx
@@ -54,101 +54,116 @@ const RequestEditModalComponent: FC<IRequestEditModalComponent> = ({
     >
       <div className={styles.container}>
         <Form className={styles.form} form={form}>
-          <Flex vertical gap={5}>
-            <Text>Исполнитель</Text>
-            <Form.Item<IRepairEditForm> name="contractor">
-              <Select
-                placeholder="Выберите исполнителя"
-                options={contractorOptions}
-                showSearch
-                optionFilterProp="label"
-              />
-            </Form.Item>
-          </Flex>
-          <Flex vertical gap={5}>
-            <Text>Срочность</Text>
-            <Form.Item<IRepairEditForm> name="urgencyId">
-              <Select
-                placeholder="Выберите срочность"
-                options={urgencyOptions}
-                showSearch
-                optionFilterProp="label"
-              />
-            </Form.Item>
-          </Flex>
-          <Flex vertical gap={5}>
-            <Text>Статус заявки</Text>
-            <Form.Item<IRepairEditForm> name="statusId">
-              <Select
-                placeholder="Выберите статус"
-                options={statusOptions}
-                showSearch
-                optionFilterProp="label"
-              />
-            </Form.Item>
-          </Flex>
-          <Flex vertical gap={5}>
-            <Text>Объект</Text>
-            <Form.Item<IRepairEditForm> name="objectId">
-              <Select
-                placeholder="Выберите объект"
-                options={objectOptions}
-                loading={isObjectsLoading}
-                showSearch
-                optionFilterProp="label"
-              />
-            </Form.Item>
-          </Flex>
-          <Flex vertical gap={5}>
-            <Text>Подрядчика</Text>
-            <Form.Item<IRepairEditForm> name="builder">
-              <Input className={styles.input} placeholder="Введите подрядчика" />
-            </Form.Item>
-          </Flex>
-          <Flex vertical gap={5}>
-            <Text>Плановая дата выполнения</Text>
-            <Form.Item<IRepairEditForm> name="planCompleteDate">
-              <DatePicker
-                style={{ width: '100%' }}
-                format={'DD.MM.YYYY'}
-                placeholder="дд.мм.гггг"
-              />
-            </Form.Item>
-          </Flex>
           <Flex vertical gap={10}>
-            <Flex align="start" gap={10}>
-              {parsedFiles.length !== 0 ? (
-                <div onClick={handleOpenSlider} className={styles.imgContainer}>
-                  <img src={`${API_URL}/uploads/${parsedFiles[parsedFiles?.length - 1]}`} />
-                </div>
-              ) : null}
-
-              <Flex className={styles.comment} vertical gap={5}>
-                <Text>Комментарий</Text>
-                <Form.Item<IRepairEditForm> name="comment">
-                  <Input.TextArea className={styles.textArea} placeholder="Введите комментарий" />
-                </Form.Item>
-              </Flex>
+            <Flex vertical gap={5}>
+              <Text>Исполнитель</Text>
+              <Form.Item<IRepairEditForm> name="contractor">
+                <Select
+                  placeholder="Выберите исполнителя"
+                  options={contractorOptions}
+                  showSearch
+                  optionFilterProp="label"
+                />
+              </Form.Item>
             </Flex>
-            <Upload
-              beforeUpload={beforeUploadFile}
-              disabled={isUploadDisabled}
-              showUploadList={false}
-            >
-              <Button icon={<UploadOutlined />}>Добавить медиа</Button>
-            </Upload>
+            <Flex vertical gap={5}>
+              <Text>Срочность</Text>
+              <Form.Item<IRepairEditForm> name="urgencyId">
+                <Select
+                  placeholder="Выберите срочность"
+                  options={urgencyOptions}
+                  showSearch
+                  optionFilterProp="label"
+                />
+              </Form.Item>
+            </Flex>
+            <Flex vertical gap={5}>
+              <Text>Статус заявки</Text>
+              <Form.Item<IRepairEditForm> name="statusId">
+                <Select
+                  placeholder="Выберите статус"
+                  options={statusOptions}
+                  showSearch
+                  optionFilterProp="label"
+                />
+              </Form.Item>
+            </Flex>
+            <Flex vertical gap={5}>
+              <Text>Объект</Text>
+              <Form.Item<IRepairEditForm> name="objectId">
+                <Select
+                  placeholder="Выберите объект"
+                  options={objectOptions}
+                  loading={isObjectsLoading}
+                  showSearch
+                  optionFilterProp="label"
+                />
+              </Form.Item>
+            </Flex>
+            <Flex vertical gap={5}>
+              <Text>Подрядчика</Text>
+              <Form.Item<IRepairEditForm> name="builder">
+                <Input className={styles.input} placeholder="Введите подрядчика" />
+              </Form.Item>
+            </Flex>
+            <Flex vertical gap={5}>
+              <Text>Плановая дата выполнения</Text>
+              <Form.Item<IRepairEditForm> name="planCompleteDate">
+                <DatePicker
+                  style={{ width: '100%' }}
+                  format={'DD.MM.YYYY'}
+                  placeholder="дд.мм.гггг"
+                />
+              </Form.Item>
+            </Flex>
           </Flex>
-          <Flex vertical gap={5}>
-            <Text>Описание проблемы</Text>
-            <Form.Item<IRepairEditForm> name="problemDescription">
-              <Input.TextArea className={styles.textArea} placeholder="Введите описание проблемы" />
-            </Form.Item>
-          </Flex>
-          <Flex vertical gap={5}>
-            <Text>Бюджет ремонта (Рублей)</Text>
-            <Form.Item<IRepairEditForm> name="repairPrice">
-              <InputNumber style={{ width: '100%', padding: 5 }} placeholder="3000" min={0} />
-            </Form.Item>
+
+          <Flex vertical gap={10}>
+            <Flex vertical gap={10}>
+              <Flex align="start" gap={10}>
+                <div className={styles.imgContainer}>
+                  {parsedFiles.length !== 0 ? (
+                    <div className={styles.img}>
+                      <img
+                        onClick={handleOpenSlider}
+                        src={`${API_URL}/uploads/${parsedFiles[parsedFiles.length - 1]}`}
+                      />
+                    </div>
+                  ) : (
+                    <div className={styles.placeholder}>Нет файла</div>
+                  )}
+                </div>
+
+                <Flex className={styles.comment} vertical gap={5}>
+                  <Text>Комментарий</Text>
+                  <Form.Item<IRepairEditForm> name="comment">
+                    <Input.TextArea className={styles.textArea} placeholder="Введите комментарий" />
+                  </Form.Item>
+                </Flex>
+              </Flex>
+              <Upload
+                beforeUpload={beforeUploadFile}
+                disabled={isUploadDisabled}
+                showUploadList={false}
+              >
+                <Button icon={<UploadOutlined />}>Добавить медиа</Button>
+              </Upload>
+            </Flex>
+            <Flex vertical gap={5}>
+              <Text>Описание проблемы</Text>
+              <Form.Item<IRepairEditForm> name="problemDescription">
+                <Input.TextArea
+                  className={styles.textArea}
+                  placeholder="Введите описание проблемы"
+                />
+              </Form.Item>
+            </Flex>
+            <Flex vertical gap={5}>
+              <Text>Бюджет ремонта (Рублей)</Text>
+              <Form.Item<IRepairEditForm> name="repairPrice">
+                <InputNumber style={{ width: '100%', padding: 5 }} placeholder="3000" min={0} />
+              </Form.Item>
+            </Flex>
           </Flex>
         </Form>
       </div>
